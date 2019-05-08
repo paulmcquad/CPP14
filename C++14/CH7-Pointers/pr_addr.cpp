ff73992b537a1fff354223df51d2b55fab5c3470
Paul McQuade <paulmcquad@gmail.com>
2019-05-08 21:11:11 +0100

    Added Pointers Chapter
    Messing around with pointers
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ C++14/CH7-Pointers/pr_addr.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+    int a = 2, b = 3, c = 4;
+    int *pa = &a;
+    int *pb = &b;
+    int *pc = &c;  
+    cout << "Value of pointer pa is: " << pa << endl;
+    cout << "Value of pointer pb is: " << pb << endl;
+    cout << "Value of pointer pc is: " << pc << endl;
+    cout << "The values of a, b, and c are: ";
+    cout << a << ", " << b << ", " << c << endl;
+    return 0;
+}
