e3215b512bc14b8f587206e58a62b311d2840c5a
Paul McQuade <paulmcquad@gmail.com>
2019-11-21 20:43:39 +0000

    Fixed output error
    output d variable
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- C++14/CH17-New Features of C++14/binary.cpp
+++ C++14/CH17-New Features of C++14/binary.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	
-     bitset<8> a(0b1111Õ0000 & 0b1001Õ0000);
-     bitset<8> b(0b1111Õ0000 | 0b1001Õ0000);
-     bitset<8> c(0b1010Õ1010 & 0b1000Õ1111);
-     bitset<8> d(0b1010Õ1010 | 0b1000Õ1111);
+
+     bitset<8> a(0b1111ï¿½0000 & 0b1001ï¿½0000);
+     bitset<8> b(0b1111ï¿½0000 | 0b1001ï¿½0000);
+     bitset<8> c(0b1010ï¿½1010 & 0b1000ï¿½1111);
+     bitset<8> d(0b1010ï¿½1010 | 0b1000ï¿½1111);
      cout << a << endl;
      cout << b << endl;
      cout << c << endl;
-     cout << e << endl;
+     cout << d << endl;
 }
