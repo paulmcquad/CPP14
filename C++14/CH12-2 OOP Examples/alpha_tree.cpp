c570c1f617648553fd30c17eacf4976f9fa109f5
Paul McQuade <paulmcquad@gmail.com>
2021-10-21 16:21:27 +0100

    CH12 - Added CMakeLists + Fix Not declared
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- C++14/CH12-2 OOP Examples/alpha_tree.cpp
+++ C++14/CH12-2 OOP Examples/alpha_tree.cpp
@@ -2,62 +2,79 @@
 #include <string>
 using namespace std;
 
-
-class Bnode {
+class Bnode
+{
 public:
-     string  val;
-     Bnode*  pLeft;
-     Bnode*  pRight;
-     Bnode(string s) {val = s; pLeft = pRight = nullptr;}
+    string val;
+    Bnode *pLeft;
+    Bnode *pRight;
+    Bnode(string s)
+    {
+        val = s;
+        pLeft = pRight = nullptr;
+    }
 };
 
-
-
-class Btree {
+class Btree
+{
 public:
-     Btree() {root = NULL; }
-     void insert(string s) 
-         {root = insert_at_sub(s, root);}
-     void print() {print_sub(root);}
+    Btree() { root = NULL; }
+    void insert(string s)
+    {
+        root = insert_at_sub(s, root);
+    }
+    void print() { print_sub(root); }
+
 private:
-     Bnode* root;
-     Bnode* insert_at_sub(string s, Bnode* p);
-     void   print_sub(Bnode* p);
+    Bnode *root;
+    Bnode *insert_at_sub(string s, Bnode *p);
+    void print_sub(Bnode *p);
 };
 
-Bnode* Btree::insert_at_sub(string s, Bnode* p) {
-    if (!p) {
+Bnode *Btree::insert_at_sub(string s, Bnode *p)
+{
+    if (!p)
+    {
         return new Bnode(s);
-    } else if (s < p->val) {
+    }
+    else if (s < p->val)
+    {
         p->pLeft = insert_at_sub(s, p->pLeft);
-    } else if (s > p->val) {
+    }
+    else if (s > p->val)
+    {
         p->pRight = insert_at_sub(s, p->pRight);
     }
     return p;
 }
 
-void Btree::print_sub(Bnode* p) {
-    if (p) {
+void Btree::print_sub(Bnode *p)
+{
+    if (p)
+    {
         print_sub(p->pLeft);
         cout << p->val << endl;
         print_sub(p->pRight);
     }
 }
 
-int main(int argc, char *argv[]) {
-    
+int main(int argc, char *argv[])
+{
+
     Btree my_tree;
     string sPrompt = "Enter a name (ENTER when done): ";
     string sInput = "";
 
-    while (true ) {
+    while (true)
+    {
         cout << sPrompt;
         getline(cin, sInput);
-        if (sInput.size() == 0) {
+        if (sInput.size() == 0)
+        {
             break;
         }
-        my_tree.insert(s);
-     }
-     cout << "Here are the names, in order." << endl;
-     my_tree.print();
+        my_tree.insert(sInput);
+    }
+    cout << "Here are the names, in order." << endl;
+    my_tree.print();
 }
