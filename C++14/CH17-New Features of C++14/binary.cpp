96ae7f0e5e34c44bac21e52ccf5ca85347fecb00
Paul McQuade <paulmcquad@gmail.com>
2021-10-22 21:22:28 +0100

    CH17 - Added CMakeLists
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- C++14/CH17-New Features of C++14/binary.cpp
+++ C++14/CH17-New Features of C++14/binary.cpp
@@ -2,12 +2,13 @@
 #include <bitset>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-     bitset<8> a(0b1111�0000 & 0b1001�0000);
-     bitset<8> b(0b1111�0000 | 0b1001�0000);
-     bitset<8> c(0b1010�1010 & 0b1000�1111);
-     bitset<8> d(0b1010�1010 | 0b1000�1111);
+     bitset<8> a(0b1111'0000 & 0b1001'0000);
+     bitset<8> b(0b1111'0000 | 0b1001'0000);
+     bitset<8> c(0b1010'1010 & 0b1000'1111);
+     bitset<8> d(0b1010'1010 | 0b1000'1111);
      cout << a << endl;
      cout << b << endl;
      cout << c << endl;
--- C++14/CH17-New Features of C++14/range_based_for.cpp
+++ C++14/CH17-New Features of C++14/range_based_for.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 #define SIZE_OF_ARRAY 5
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
      int arr[SIZE_OF_ARRAY];
      int total = 0;
@@ -13,7 +14,8 @@ int main(int argc, char *argv[]) {
      // For each element, prompt for a value,
      //   store, and add to total.
      //
-     for (int& n : arr) {
+     for (int &n : arr)
+     {
           cout << "Enter array value: ";
           cin >> n;
           total += n;
@@ -22,7 +24,8 @@ int main(int argc, char *argv[]) {
 
      // Print each element.
      //
-     for (int n : arr) {
+     for (int n : arr)
+     {
           cout << n << endl;
      }
 
@@ -32,12 +35,14 @@ int main(int argc, char *argv[]) {
 
      // Set each element to 0.
      //
-     for (int& n : arr) {
+     for (int &n : arr)
+     {
           n = 0;
      }
 
      cout << "Here are the values: ";
-     for (int n : arr) {
+     for (int n : arr)
+     {
           cout << n << endl;
      }
      return 0;
